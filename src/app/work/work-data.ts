e8dbd99005574cced650a7697ac3d3d0d3bfc3fe
akihiro.tanaka <big.a.0816@gmail.com>
2017-11-09 08:38:29 +0900

    fix style & image

--- src/app/work/work-data.ts
+++ src/app/work/work-data.ts
@@ -1,14 +1,14 @@
 export const WORKS = [
   {
     name: '歴食',
-    img: './assets/img/work/rekishoku.png',
+    img: './assets/img/work/rekishoku.jpg',
     skills: ['UI', 'Front', 'Server'],
     description: '夏目漱石が愛した和菓子、伊達政宗が考えたお餅、江戸っ子に人気の鍋など、”ストーリーのある食”を”食べに行ける店”と合わせて紹介する歴史系メディア。',
     url: 'http://rekishoku.jp/'
   },
   {
     name: '新幹線ゲーム',
-    img: './assets/img/work/shinkansen.png',
+    img: './assets/img/work/shinkansen.jpg',
     skills: ['Front'],
     description: '趣味で作成した、駄菓子屋によくあった新幹線ゲームです。 ブラウザ上で動きます。',
     url: 'http://tanakas.org/games/shinkansen/'
